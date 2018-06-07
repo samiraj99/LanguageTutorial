754cc7dc01baa6ba7ce8d906ae442d68a599bd9a
Samiraj Shirsat <samirajshirsat@gmail.com>
2018-06-07 13:36:21 +0530

    Updating Splash Activity
    link splash activity with next login page.

--- app/src/main/java/com/foslipy/languagetutorial/Splash.java
+++ app/src/main/java/com/foslipy/languagetutorial/Splash.java
@@ -1,5 +1,7 @@
 package com.foslipy.languagetutorial;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,23 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new CountDownTimer(100, 50000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent login = new Intent(Splash.this,Login.class);
+                startActivity(login);
+                finish();
+            }
+        }.start();
     }
 }
