d46aa7ced207c4e21386fb2f85c25fbaa122a89c
Samiraj Shirsat <samirajshirsat@gmail.com>
2018-06-08 17:14:29 +0530

    Adding logout feature.

--- app/src/main/java/com/foslipy/languagetutorial/Home.java
+++ app/src/main/java/com/foslipy/languagetutorial/Home.java
@@ -1,18 +1,22 @@
 package com.foslipy.languagetutorial;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout homedrawerlayout;
     private ActionBarDrawerToggle homedrawertoggle;
-
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +34,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmest_container_frame_layout,new home_fragment()).commit();
         navigationView.setCheckedItem(R.id.home_menu);
 
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 
 
@@ -62,10 +67,14 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmest_container_frame_layout,new ranking_fragment()).commit();
                 break;
             case R.id.Logout_menu:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragmest_container_frame_layout,new logout_fragment()).commit();
+               firebaseAuth.signOut();
+               finish();
+                Intent log=new Intent(Home.this,Login.class);
+                startActivity(log);
                 break;
         }
 
+        homedrawerlayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
