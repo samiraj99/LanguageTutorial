6626c3312693a65f0bd0eed967a895768d70b72a
Samiraj Shirsat <samirajshirsat@gmail.com>
2018-06-10 22:47:07 +0530

    Profile Activity Updated

--- app/src/main/java/com/foslipy/languagetutorial/ProfileActivity.java
+++ app/src/main/java/com/foslipy/languagetutorial/ProfileActivity.java
@@ -1,14 +1,18 @@
 package com.foslipy.languagetutorial;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,11 +24,12 @@ import com.google.firebase.database.ValueEventListener;
 public class ProfileActivity extends AppCompatActivity {
 
 DatabaseReference databaseReference;
-TextView Image_first_name,First_name,Last_name,Email,Occupation;
+TextInputEditText First_name,Last_name,Occupation;
+TextView Image_first_name,Email;
 FirebaseUser user;
 String uid;
 ProgressDialog dialog;
-Button Edit;
+TextView Btn_Edit,Btn_Done;
 String User_first_name,User_last_name,User_email,User_occupation;
 @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +41,11 @@ String User_first_name,User_last_name,User_email,User_occupation;
         Image_first_name=findViewById(R.id.Profile_Image_first_name);
         First_name=findViewById(R.id.Profile_first_name);
         Last_name=findViewById(R.id.Profile_last_name);
-        Email=findViewById(R.id.Profile_email);
+        Email=findViewById(R.id.Profile_image_email);
         Occupation=findViewById(R.id.Profile_occupation);
-        Edit=findViewById(R.id.Profile_button_edit);
+        Btn_Done=findViewById(R.id.Profile_button_done);
+        Btn_Edit=findViewById(R.id.Profile_button_edit);
+
         dialog=new ProgressDialog(this);
 
         dialog.setMessage("Loading..!");
@@ -68,17 +75,60 @@ String User_first_name,User_last_name,User_email,User_occupation;
             }
         });
 
-        Edit.setOnClickListener(new View.OnClickListener() {
+        Btn_Edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent edit = new Intent(ProfileActivity.this,EditProfile.class);
-                edit.putExtra("First_Name",User_first_name);
-                edit.putExtra("Last_Name",User_last_name);
-                edit.putExtra("Email",User_email);
-                edit.putExtra("Occupation",User_occupation);
-                startActivity(edit);
+                First_name.setEnabled(true);
+                Last_name.setEnabled(true);
+                Occupation.setEnabled(true);
+                Btn_Done.setVisibility(View.VISIBLE);
+                Btn_Edit.setVisibility(View.INVISIBLE);
             }
         });
 
+        Btn_Done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                User_first_name=First_name.getText().toString();
+                User_last_name=Last_name.getText().toString();
+                User_occupation=Occupation.getText().toString();
+
+                if(TextUtils.isEmpty(User_first_name))
+                {
+                    First_name.setError("Fields can't be Empty");
+                }else if(TextUtils.isEmpty(User_last_name))
+                {
+                    Last_name.setError("Fields can't be Empty");
+                }else if(TextUtils.isEmpty(User_occupation))
+                {
+                    Last_name.setError("Fields can't be Empty");
+                }else {
+                    RegistrationData data = new RegistrationData(User_first_name, User_last_name, User_occupation, User_email);
+                    databaseReference.child(uid).child("Info").setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful())
+                            {
+
+                                First_name.setEnabled(false);
+                                Last_name.setEnabled(false);
+                                Occupation.setEnabled(false);
+                                Btn_Done.setVisibility(View.INVISIBLE);
+                                Btn_Edit.setVisibility(View.VISIBLE);
+                                First_name.setFocusable(false);
+                                Last_name.setFocusable(false);
+                                Occupation.setFocusable(false);
+                                Toast.makeText(ProfileActivity.this, "Profile has been updated", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(ProfileActivity.this, "Check Internet Connection", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
     }
 }
