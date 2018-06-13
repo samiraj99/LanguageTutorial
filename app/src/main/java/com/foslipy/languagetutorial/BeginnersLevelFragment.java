5aa7eb5382e53efe03e2b4095a712c097c23d8d0
mahadikPrathamesh <36527725+mahadikPrathamesh@users.noreply.github.com>
2018-06-13 12:50:52 +0530

    Merge branch 'master' of C:\Users\Prathamesh\AndroidStudioProjects\LanguageTutorial with conflicts.

--- app/src/main/java/com/foslipy/languagetutorial/BeginnersLevelFragment.java
+++ app/src/main/java/com/foslipy/languagetutorial/BeginnersLevelFragment.java
@@ -1,5 +1,6 @@
 package com.foslipy.languagetutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BeginnersLevelFragment extends Fragment {
     View view;
@@ -15,6 +17,7 @@ public class BeginnersLevelFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.beginners_fragment_layout, container, false);
+
         return view;
 
     }
--- app/src/main/java/com/foslipy/languagetutorial/Splash.java
+++ app/src/main/java/com/foslipy/languagetutorial/Splash.java
@@ -5,29 +5,35 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class Splash extends AppCompatActivity {
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        new CountDownTimer(2000, 500) {
-            @Override
-            public void onTick(long l) {
+       }
 
-            }
+       @Override
+       protected void onStart() {
+           super.onStart();
+           new CountDownTimer(3000, 500) {
+               @Override
+               public void onTick(long l) {
 
-            @Override
-            public void onFinish() {
-                Intent login = new Intent(Splash.this,Login.class);
-                startActivity(login);
-                finish();
-            }
-        }.start();
-    }
-}
+               }
+
+               @Override
+               public void onFinish() {
+                   Intent login = new Intent(Splash.this,Login.class);
+                   startActivity(login);
+                   finish();
+               }
+           }.start();
+       }
+
+   }
+
+
+
